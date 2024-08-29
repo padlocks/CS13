238712a866082c337a624f1f5a4d2e919c06013a
Pascal Young <pascalyoung03@gmail.com>
2024-08-28 17:12:34 -0700

    convert to switch statement

--- SoloDiceDuel.java
+++ SoloDiceDuel.java
@@ -22,71 +22,66 @@ public class SoloDiceDuel {
 		if (input.equals("")) {
 			// Start game loop
 			playing = true;
-			while (playing) {
-				System.out.println("Points: " + points + " Banked: " + bank);
-				System.out.println("r to roll, b to bank current points, s to stop");
-
-				try {
-					input = scanner.next();
-
-					// if (!validInputs.includes(input)) {
-					// 	System.out.println("Not a valid input.");
-					// 	continue;
-					// }
-				} catch (InputMismatchException e) {
-					System.out.println("Must be a letter.");
-					continue;
-				}
-
-				if (input.equals("r")) {
-					// Rolling
-					int roll = rollDice();
-
-					// Initial roll
-					if (points == 0) {
-						handleInitialRoll(roll);
-					}
-
-					if (roll == lastRoll) {
-						points += 5;
-						rolledPoint = true;
-					}
-
-					// Set last roll
-					lastRoll = roll;
-
-					if (roll == 7 && !rolledPoint) {
-						points -= 10;
-						System.out.println("Rolled a 7 before the Point.");
-						continue;
-					}
-
-					System.out.println("Rolled: " + roll + " Points: " + points);
-				}
-				else if (input.equals("b")) {
-					bankPoints();
-					System.out.println("You have " + bank + " banked points.\nWould you like to play Double or Nothing?");
-					System.out.println("Rolling a 7 or 11 will double your banked points.\nRolling a 2, 3, or 12 will cause you to lose your banked points.");
-					System.out.println("Would you like to play? y or n");
-					input = scanner.next();
-
-					if (input.equals("y")) {
-						doubleOrNothing();
-					}
-
-					continue;
-				}
-				else if (input.equals("s")) {
-					System.out.println("Game has stopped. You lose!");
-					break;
-				}
-
-				if (points == 100) {
-					// Win condition met
-					System.out.println("You have reached 100 points, you win!");
-					break;
-				}
-			}	
+                    OUTER:
+                    while (playing) {
+                        System.out.println("Points: " + points + " Banked: " + bank);
+                        System.out.println("r to roll, b to bank current points, s to stop");
+                        try {
+                            input = scanner.next();
+                            
+                            // if (!validInputs.includes(input)) {
+                            // 	System.out.println("Not a valid input.");
+                            // 	continue;
+                            // }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Must be a letter.");
+                            continue;
+                        }
+                        switch (input) {
+                            case "r":
+                                // Rolling
+                                int roll = rollDice();
+                                // Initial roll
+                                if (points == 0) {
+                                    handleInitialRoll(roll);
+                                }
+                                if (roll == lastRoll) {
+                                    points += 5;
+                                    rolledPoint = true;
+                                }
+                                // Set last roll
+                                lastRoll = roll;
+                                if (roll == 7 && !rolledPoint) {
+                                    points -= 10;
+                                    System.out.println("Rolled a 7 before the Point.");
+                                    continue;
+                                }
+                                System.out.println("Rolled: " + roll + " Points: " + points);
+                                break;
+                            case "b":
+                                bankPoints();
+                                System.out.println("You have " + bank + " banked points.\nWould you like to play Double or Nothing?");
+                                System.out.println("Rolling a 7 or 11 will double your banked points.\nRolling a 2, 3, or 12 will cause you to lose your banked points.");
+                                System.out.println("Would you like to play? y or n");
+                                input = scanner.next();
+                                
+                                if (input.equals("y")) {
+                                    doubleOrNothing();
+                                }
+                                
+                                continue;
+                            case "s":
+                                System.out.println("Game has stopped. You lose!");
+                                break OUTER;
+                            default:
+                                continue;
+                        }
+                        if (points == 100) {
+                            // Win condition met
+                            System.out.println("You have reached 100 points, you win!");
+                            break;
+                        }
+                    }	
 		}
 	}
 
